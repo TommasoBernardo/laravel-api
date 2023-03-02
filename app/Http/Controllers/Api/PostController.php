58376acb2f103c7c66c914a18d10f612fa66a5fa
TommasoBernardo <tommasob1210@gmail.com>
2023-03-02 17:26:00 +0100

    update and create api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,8 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //
+    public function index()
+    {
+        $posts = Post::paginate(10);
+        return response()->json([
+            'succes' => true,
+            'results' => $posts
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return response()->json([
+            'succes' => true,
+            'results' => $post
+        ]);
+    }
 }
